f83d3c203b4003854523d29ec165c2b39268f302
Manviya <monica.manviya@capgemini.com>
2020-12-23 11:26:30 +0530

    my-assignment

---
+++ README.md
@@ -0,0 +1,3 @@
+npm install
+Press Ctrl+Shift+B and if all went well a new file will be created (main.js). In order to see the output we need to feed it into node command.
+node main.js
---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+var fs = require('fs');
+var csv = fs.readFileSync('BankChurners.csv', 'utf8');
+var stats = fs.statSync("BankChurners.csv");
+var allTextLines = csv.split('\n');
+console.log('Column or field names present in the CSV :', allTextLines[0]);
+console.log('Total size in bytes of the file:', stats.size);
+console.log('Total number of rows:', allTextLines.length);
